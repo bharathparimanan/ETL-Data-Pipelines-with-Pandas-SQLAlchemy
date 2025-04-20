cad8aee5321dcbc39c6775705468a2bba5dab5ec
bharathparimanan@gmail.com <bharathparimanan@gmail.com>
2025-04-20 21:47:29 +0100

    test case updated for data pipeline

--- test/test_etl.py
+++ test/test_etl.py
@@ -1,7 +1,6 @@
 import os.path
 import sys
 import logging
-import pytest
 import pandas as pd
 
 #Add project path to python root path
@@ -9,9 +8,24 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
 from src.etl import etl_parquet, extract
 
+def test_data_pipeline():
+    try:
+        logging.info(
+            f"run etl data pipline"
+        )
+        extract()
+        return None
+    except Exception as e:
+        logging.error(
+            f"ERROR occurred while running data pipeline"
+        )
+        return e
 
 def test_sql_data():
     try:
+        logging.info(
+            f"check the loaded dataframe is instance of pandas dataframe"
+        )
         result = etl_parquet()
         assert isinstance(result, pd.DataFrame)
         return None
